b1f9d764ebc50d0aac9815e5704a8fcc5abdb275
Daniel <dfquack08@gmail.com>
2026-07-17 20:12:08 -0600

    Implement task creation message

--- app/src/main/java/com/example/dsm_investigacion01/MainActivity.kt
+++ app/src/main/java/com/example/dsm_investigacion01/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.example.dsm_investigacion01
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.dsm_investigacion01.databinding.ActivityMainBinding
+import com.example.dsm_investigacion01.R.*
+import com.google.android.material.color.MaterialColors
 
 class MainActivity : AppCompatActivity() {
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.addTaskButton.setOnClickListener {
+            val taskName = binding.taskTextbox.text
+            if (taskName.isNullOrBlank()) {
+                binding.taskMessage.text = getString(string.task_message_failure)
+                binding.taskMessage.setTextColor(ContextCompat.getColor(this, color.error))
+                binding.taskMessage.visibility = View.VISIBLE
+            } else {
+                binding.taskMessage.text = getString(string.task_message_success)
+                binding.taskMessage.setTextColor(ContextCompat.getColor(this, color.success))
+                binding.taskMessage.visibility = View.VISIBLE
+            }
+        }
     }
 }
